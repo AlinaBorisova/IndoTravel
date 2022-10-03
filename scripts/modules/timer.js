50028a75fc570591359cde163501ac53fb789ff7
Алина Борисова <b-alina1999.borisova@yandex.ru>
2022-10-03 14:06:35 +0300

    Timer

--- scripts/modules/timer.js
+++ scripts/modules/timer.js
@@ -9,21 +9,39 @@ export const getElements = () => {
 
 export const timer = deadline => {
   const daysCount= document.querySelector('.timer__count_days');
-  const hoursCount = document.querySelector('.timer__count_hours');
-  const minutesCount = document.querySelector('.timer__count_minutes');
   const daysUnits = document.querySelector('.timer__units_days');
+  const hoursCount = document.querySelector('.timer__count_hours');
   const hoursUnits = document.querySelector('.timer__units_hours');
+  const minutesCount = document.querySelector('.timer__count_minutes');
   const minutesUnits = document.querySelector('.timer__units_minutes');
 
+    // доп. задание с секундами
+  const seconds = document.createElement('p');
+  seconds.classList.add('timer__item', 'timer__item_seconds');
+  const secondsCount = document.createElement('span');
+  secondsCount.classList.add('timer__count', 'timer__count_seconds');
+  const secondUnits = document.createElement('span');
+  secondUnits.classList.add('timer__units', 'timer__units_seconds');
+
+  const itemSeconds = document.querySelector('.timer');
+  seconds.append(secondsCount, secondUnits);
+  seconds.style.display = 'none'
+  itemSeconds.append(seconds);
+
   const getTimeRemaining = () => {
-    const dateStop = new Date(deadline).getTime();
+    // Доп задиние с временем по Гринвичу +03:00
+    const dateStop = new Date(deadline);
+    dateStop.setHours(dateStop.getHours() + 3);
+
+    // const dateStop = new Date(deadline).getTime();     Изначальный вариант с местным временем
     const dateNow = Date.now();
     const timeRemaining = dateStop - dateNow;
+    const seconds = Math.floor(timeRemaining / 1000 % 60);
     const minutes= Math.floor(timeRemaining / 1000 / 60 % 60);
     const hours = Math.floor(timeRemaining / 1000 / 60 / 60 % 24);
     const days= Math.floor(timeRemaining / 1000 / 60 / 60 / 24);
 
-    return {timeRemaining, minutes, hours, days};
+    return {timeRemaining, seconds, minutes, hours, days};
   };
     
   const start = () => {
@@ -31,6 +49,7 @@ export const timer = deadline => {
     daysCount.textContent = timer.days;
     hoursCount.textContent = timer.hours;
     minutesCount.textContent = timer.minutes;
+    
     if(hoursCount.textContent < 10) {
       hoursCount.textContent = "0" + hoursCount.textContent;
     };
@@ -48,6 +67,18 @@ export const timer = deadline => {
     minutesUnits.textContent = sklonenie(minutesCount.textContent, ['минута', 'минуты', 'минут']);
 
     const intervalId = setTimeout(start, 60000);
+
+    // доп. задание с секундами
+    if (timer.days = 0 && timer.hours <= 24) {
+      clearTimeout(timer.intervalId);
+      setTimeout(start, 1000);
+      const itemDays = document.querySelector('.timer__item_days');
+      itemDays.style.display = 'none'
+      
+      secondsCount.textContent = timer.seconds;
+      secondUnits.textContent = sklonenie(secondUnits.textContent, ['секунда', 'секунды', 'секунд']);
+      seconds.style.display = null;
+    };
 
     if(timer.timeRemaining <= 0) {
       clearTimeout(intervalId);
